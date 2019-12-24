78769e10084f8e0f2a7f0c499ec0a572ad87abf2
unknown <scolkg@gmail.com>
2019-12-24 18:18:02 +0900

    2

--- 3.react-webgame/2.끝말잇기/webpack.config.js
+++ 3.react-webgame/2.끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   name: 'word-relay-setting',
@@ -19,13 +20,26 @@ module.exports = {
     rules: [{
       test: /\.jsx?/, 
       loader: 'babel-loader',
+      
+      // plugin들의 모음이 presets이다.
       options: {
-        presets: [ '@babel/preset-env', '@babel/preset-react' ],
-        plugins: [ '@babel/plugin-proposal-class-properties' ],
+        presets: [ ['@babel/preset-env', {
+          targets: {
+            browsers: ['> 5% in KR', 'last 2 chrome versions'],
+          },
+          debug: true,
+        }], 
+        '@babel/preset-react' 
+      ],
+        plugins: [],
       },
     }],
-
   },
+
+  // 추가할 확장기능들을 적용해서~
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+  ],
 
   // 출력으로 뺀다~
   output: {
