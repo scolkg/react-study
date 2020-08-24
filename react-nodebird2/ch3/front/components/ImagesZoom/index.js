c6c260dafbd8af9afcbb6a0f998e517162806bc8
unknown <scolkg@gmail.com>
2020-08-24 17:59:19 +0900

    20200824 2

---
+++ react-nodebird2/ch3/front/components/ImagesZoom/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Slick from 'react-slick';
+import { Overlay, Header, CloseBtn, SlickWrapper, ImgWrapper, Indicator, Global } from './styles';
+
+// 바로 ImagesZoom.js 파일을 만들지 않고 ImagesZoom 폴더안에다가 index.js를 넣는 이유는
+// 스타일드컴포넌트를 같은 js에 쓰면 너무 길어지니 스타일드는 따로 두고 import하여 쓰기 위함.
+const ImagesZoom = ({ images, onClose }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  return (
+    <Overlay>
+      <Global />
+      <Header>
+        <h1>상세 이미지</h1>
+        <CloseBtn onClick={onClose} />
+      </Header>
+      <SlickWrapper>
+        <div>
+          <Slick
+            initialSlide={0}
+            afterChange={(slide) => setCurrentSlide(slide)}
+            infinite
+            arrows={false}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            {images.map((v) => (
+              <ImgWrapper key={v.src}>
+                <img src={v.src} alt={v.src} />
+              </ImgWrapper>
+            ))}
+          </Slick>
+          <Indicator>
+            <div>
+              {currentSlide + 1}
+              {' '}
+              /
+              {images.length}
+            </div>
+          </Indicator>
+        </div>
+      </SlickWrapper>
+    </Overlay>
+  );
+};
+
+ImagesZoom.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.shape({
+    src: PropTypes.string,
+  })).isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ImagesZoom;
