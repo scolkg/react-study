307d5cf5ca291e41b8c19d2a64577a557619a93b
unknown <scolkg@gmail.com>
2020-08-31 18:02:54 +0900

    20200831

--- react-nodebird2/ch4/front/store/configureStore.js
+++ react-nodebird2/ch4/front/store/configureStore.js
@@ -4,8 +4,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from '../reducers';
 
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+  console.log(action);
+
+  return next(action);
+};
+
 const configureStore = () => {
-  const middlewares = [];
+  const middlewares = [ loggerMiddleware ];
   const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(applyMiddleware(...middlewares))
