c3b3a58d84b6cd576da079f7603143bf68f1f4cd
Geon Kim <scolkg@gmail.com>
2020-09-11 01:15:12 +0900

    20200911

--- react-nodebird2/ch4/front/store/configureStore.js
+++ react-nodebird2/ch4/front/store/configureStore.js
@@ -1,8 +1,10 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, createStore, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from '../reducers';
+import rootSaga from '../sagas';
 
 const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
   console.log(action);
@@ -11,12 +13,14 @@ const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
 };
 
 const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
   const middlewares = [ loggerMiddleware ];
   const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(applyMiddleware(...middlewares))
   
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
