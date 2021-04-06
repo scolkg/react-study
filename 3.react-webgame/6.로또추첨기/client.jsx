ec64e610e295d598defd634a2d3e2be682797426
scolkg <scolkg@gmail.com>
2021-04-07 08:59:45 +0900

    a

--- 3.react-webgame/6.로또추첨기/client.jsx
+++ 3.react-webgame/6.로또추첨기/client.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { hot } from 'react-hot-loader/root';
+import React from "react";
+import ReactDom from "react-dom";
 
-import Lotto from './Lotto';
-//import Lotto from './LottoHooks';
+// import Lotto from './Lotto';
+import Lotto from "./LottoHooks";
 
-const Hot = hot( Lotto );
+const Hot = hot(Lotto);
 
-ReactDom.render( <Hot />, document.querySelector("#root"));
+ReactDom.render(<Hot />, document.querySelector("#root"));
---
+++ 3.react-webgame/6.로또추첨기/dist/app.js
@@ -0,0 +1,111 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/dist";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./client.jsx":
+/*!********************!*\
+  !*** ./client.jsx ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError [ERR_PACKAGE_PATH_NOT_EXPORTED]: No \\\"exports\\\" main resolved in D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\helper-compilation-targets\\\\package.json\\n    at applyExports (internal/modules/cjs/loader.js:491:9)\\n    at resolveExports (internal/modules/cjs/loader.js:507:23)\\n    at Function.Module._findPath (internal/modules/cjs/loader.js:635:31)\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:953:27)\\n    at Function.Module._load (internal/modules/cjs/loader.js:842:27)\\n    at Module.require (internal/modules/cjs/loader.js:1026:19)\\n    at require (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\debug.js:8:33)\\n    at Module._compile (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1158:10)\\n    at Module.load (internal/modules/cjs/loader.js:986:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:879:14)\\n    at Module.require (internal/modules/cjs/loader.js:1026:19)\\n    at require (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\index.js:11:14)\\n    at Module._compile (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1158:10)\\n    at Module.load (internal/modules/cjs/loader.js:986:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:879:14)\\n    at Module.require (internal/modules/cjs/loader.js:1026:19)\\n    at require (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:161:20)\\n    at requireModule (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js:165:12)\\n    at loadPreset (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js:83:17)\\n    at createDescriptor (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:154:9)\\n    at D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:109:50\\n    at Array.map (<anonymous>)\\n    at createDescriptors (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:109:29)\\n    at createPresetDescriptors (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:101:10)\\n    at D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:58:104\\n    at cachedFunction (D:\\\\Work\\\\react\\\\react-study\\\\3.react-webgame\\\\6.로또추첨기\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\caching.js:62:27)\");\n\n//# sourceURL=webpack:///./client.jsx?");
+
+/***/ }),
+
+/***/ 0:
+/*!**********************!*\
+  !*** multi ./client ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__(/*! ./client */\"./client.jsx\");\n\n\n//# sourceURL=webpack:///multi_./client?");
+
+/***/ })
+
+/******/ });
